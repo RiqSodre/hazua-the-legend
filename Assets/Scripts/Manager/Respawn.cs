0abf5e162e571a1efa2d6fead303df9c8da7838d
Ricardo Sodre <riqsodre@gmail.com>
2018-05-28 15:21:32 -0300

    Criação do Respawn

---
+++ Assets/Scripts/Manager/Respawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Respawn : MonoBehaviour {
+
+	void Awake () {
+	}
+
+    void OnCollisionEnter2D(Collision2D colidir)
+    {
+        if (colidir.gameObject.tag == "Respawn")
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    void Update () {
+		
+	}
+}
