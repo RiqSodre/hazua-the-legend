9b6f2034d2e12af8952582842be4f9ba6bbaee49
Ricardo Sodre <riqsodre@gmail.com>
2018-05-31 21:04:16 -0300

    HUD de vida corrigido

--- Assets/Scripts/Manager/Health.cs
+++ Assets/Scripts/Manager/Health.cs
@@ -8,11 +8,14 @@ public class Health : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        GameControl.health += 1;
-        if(GameControl.health > 5)
+        if(GameControl.health >= 5)
         {
             GameControl.health += 0;
         }
-        Destroy(heart);
+        else
+        {
+            GameControl.health += 1;
+            Destroy(heart);
+        }
     }
 }
